749d63b6b34f8fd9d10afe50702974dd65e36a87
Jessica Maria Loredo <jesi.thegreat@gmail.com>
2018-07-07 19:25:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.shout())
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log(string.whisper())
+  console.log(string.toLowerCase())
 }
