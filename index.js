b62d2ca9996fbe5471a23efaa6e8deba9fc50bc0
Jessica Maria Loredo <jesi.thegreat@gmail.com>
2018-07-07 20:12:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,16 +28,17 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
    if (string === string.toLowerCase()) {
-     return "I can't hear you!" }
-     
+     return "I can't hear you!" 
+   }   
+   
    else if (string === string.toUpperCase())  {
-     
+     return "YES INDEED!"
    }
-     return ""
-     
+   
+   else if (string === "I love you, Grandma.") {
+    return "I love you, too."
    }
 }
-
 
 
 
